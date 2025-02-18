c30643589ef84579d3e9900c315eb79e4b5f01f1
susanta2022 <susanta@adansa.com>
2025-02-18 13:27:34 +0530

    sfg

--- test_math_operations.py
+++ test_math_operations.py
@@ -2,7 +2,7 @@ import pytest
 from math_operations import add
 
 def test_add_positive_numbers():
-    assert add(2, 3) == 5
+    assert add(2, 3) == 10
 
 def test_add_negative_numbers():
     assert add(-1, -1) == -2
